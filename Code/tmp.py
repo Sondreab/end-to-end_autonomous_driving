ddd3c6f85cd7208b70161b119977ffeca66c4eae
Filip Schjerven <fschjerven@gmail.com>
2019-04-01 23:12:07 +0200

    Fjernet img fra img-paths

--- Code/tmp.py
+++ Code/tmp.py
@@ -12,9 +12,9 @@ def fix_path_names():
     path = os.getcwd().split(os.sep)[:-1] + ["driving_log.csv"]
     path_str = (os.sep).join(path) 
     data = pd.read_csv(path_str, header=None) 
-    front = [(os.sep).join(x.split(os.sep)[-2:]) for x in data[0]]
-    left = [(os.sep).join(x.split(os.sep)[-2:]) for x in data[1]]
-    right = [(os.sep).join(x.split(os.sep)[-2:]) for x in data[2]]
+    front = [x.split(os.sep)[-1] for x in data[0]]
+    left = [x.split(os.sep)[-1] for x in data[1]]
+    right = [x.split(os.sep)[-1] for x in data[2]]
     data[0] = front
     data[1] = left
     data[2] = right
