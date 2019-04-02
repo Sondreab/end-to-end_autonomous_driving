8ea006004925513a2bf7abb4920c2af7d35a44d4
filsch <fschjerven@gmail.com>
2019-04-02 11:20:53 +0200

    trained a couple of models

--- Code/Keras_test.py
+++ Code/Keras_test.py
@@ -9,7 +9,7 @@ from tensorflow import set_random_seed
 set_random_seed(2)
 
 from keras import optimizers
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Dense, Flatten, Dropout, BatchNormalization
 from keras.models import load_model, Sequential
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.preprocessing.image import img_to_array, load_img
@@ -24,21 +24,21 @@ def model(load, saved_model, shape):
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation="relu", input_shape=shape))
     model.add(MaxPooling2D())
-    #model.add(BatchNormalization(axis=1))
+    model.add(BatchNormalization(axis=2))
     model.add(Conv2D(64, (3, 3), activation="relu"))
     model.add(MaxPooling2D())
-    #model.add(BatchNormalization(axis=1))
+    model.add(BatchNormalization(axis=2))
     model.add(Conv2D(128,(3, 3), activation="relu"))
     model.add(MaxPooling2D())
-    #model.add(BatchNormalization(axis=1))
+    model.add(BatchNormalization(axis=2))
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1, activation='linear'))
     
-    optim = optimizers.Adam(lr = 0.01, decay = 1e-5)
+    optim = optimizers.Adam()
     model.compile(loss="mse", optimizer=optim)
     return model
 
@@ -56,13 +56,11 @@ def split_data(nr_pts):
     idx = np.arange(nr_pts)
     np.random.shuffle(idx)
     
-    tr = int(np.floor(nr_pts*0.75))
-    va = int(np.floor(nr_pts*0.9))
+    tr = int(np.floor(nr_pts*0.8))
     train    = idx[:tr]
-    validate = idx[tr:va]
-    test     = idx[va:]
+    validate = idx[tr:]
     
-    return train, validate, test
+    return train, validate
 
 #Change if augmentation is performed
 def _generator(batch_size, X, y, shape, path):
@@ -80,24 +78,25 @@ def train(path,log):
     front, left, right = np.loadtxt(log, delimiter=",", usecols=[0,1,2], dtype="str", unpack=True)
     angle, forward, backward, speed = np.loadtxt(log, delimiter=",", usecols=[3,4,5,6], unpack=True)
 
-    train, validate, test = split_data(len(front))
+    train, validate = split_data(len(front))
     net  = model(load=False, saved_model=None, shape=shape)
-    X, y = front[train], np.divide(angle[train],2) + 0.5
+    X, y = front[train], angle[train]
     
-    X_val, y_val = front[validate], np.divide(angle[validate],2) + 0.5
+    X_val, y_val = front[validate], angle[validate]
     
     net.fit_generator(generator        = _generator(64, X, y, shape, path),
-                      validation_data  = _generator(10,X_val, y_val, shape, path),
-                      validation_steps = 10, 
-                      epochs = 3, steps_per_epoch=10)
-    net.save('testmodel.h5')
+                      validation_data  = _generator(20, X_val, y_val, shape, path),
+                      validation_steps = 20, 
+                      epochs = 2, steps_per_epoch=100)
+    net.save('testmodel3.h5')
     
 
 
 if __name__ == "__main__":
     path = os.getcwd().split(os.sep)[:-1]
     log = path + ["driving_log.csv"]
-    train((os.sep).join(path), (os.sep).join(log))
+    img = path + ["IMG"]
+    train((os.sep).join(img), (os.sep).join(log))
 
 
 
