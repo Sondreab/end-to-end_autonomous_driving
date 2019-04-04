9303c80bff533719321258f9008a8ce340b533db
filsch <fschjerven@gmail.com>
2019-04-04 13:47:13 +0200

    La til crop funksjon

--- Code/Keras_test.py
+++ Code/Keras_test.py
@@ -11,8 +11,9 @@ set_random_seed(2)
 from keras import optimizers
 from keras.layers import Dense, Flatten, Dropout, BatchNormalization
 from keras.models import load_model, Sequential
-from keras.layers.convolutional import Conv2D, MaxPooling2D     
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.preprocessing.image import img_to_array, load_img
+from matplotlib import pyplot as plt
 import numpy as np
 import os
 
@@ -50,8 +51,15 @@ def flip_axis(img,axis):
             new[:,i,:] = img[:,dim-i,:]
     return new
 
+def crop(img):
+    shape = img.shape
+    new = np.zeros(shape)
+    for i in np.arange(int(shape[0]/3)):
+        new[i,:,:] = 0
+    return new
+
 # load image into known format.
-def image_handling(path, steering_angle, flip, augment=0, shape=(100,200)):
+def image_handling(path, steering_angle, flip, shape, crop_bool=True):
     """ Image handling """
     image = load_img(path, target_size=shape)
         
@@ -59,8 +67,8 @@ def image_handling(path, steering_angle, flip, augment=0, shape=(100,200)):
     if flip: 
         img = flip_axis(img, 1)
         steering_angle = -steering_angle
-    
-    return img, steering_angle
+            
+    return crop(img,crop_bool), steering_angle
     
 def split_data(nr_pts):
     idx = np.arange(nr_pts)
@@ -79,11 +87,11 @@ def sample_idx(batch_size, y, proportion):
     while i < batch_size:
         candidate = np.random.randint(0,data_num,1)[0]
         #Image of driving forward
-        if y[candidate] == 0 and np.random.ranf(1) > proportion:
+        if y[candidate] == 0 and np.random.ranf(1) < (1-proportion)*0.5:
             idx[i] = candidate
             i+=1
         #Image of turning
-        elif np.random.ranf(1) < proportion:
+        elif y[candidate] != 0 and np.random.ranf(1) < proportion:
             idx[i]  = candidate
             flip[i] = np.random.binomial(1,0.5)
             i+=1
@@ -99,9 +107,9 @@ def _generator(batch_size, X, y, shape, path, proportion):
             x, angle = image_handling(path + os.sep + X[i], y[i], flip_bool, shape=shape)
             batch_x.append(x)
             batch_y.append(angle)
-        print("Left: ",np.sum(batch_y < 0))
-        print("Forward: ",np.sum(batch_y == 0))
-        print("Right: ",np.sum(batch_y > 0))
+        #print("Left: ",np.sum(np.less(batch_y,0)))
+        #print("Forward: ",np.sum(np.equal(batch_y,0)))
+        #print("Right: ",np.sum(np.greater(batch_y,0)))
         yield np.array(batch_x), np.array(batch_y)
               
             
@@ -110,7 +118,7 @@ def train(path,log):
     front, left, right = np.loadtxt(log, delimiter=",", usecols=[0,1,2], dtype="str", unpack=True)
     angle, forward, backward, speed = np.loadtxt(log, delimiter=",", usecols=[3,4,5,6], unpack=True)
 
-    proportion = np.sum(angle == 0)
+    proportion = np.sum(angle == 0)/len(angle)
     
     train, validate = split_data(len(front))
     net  = model(load=False, saved_model=None, shape=shape)
@@ -121,11 +129,13 @@ def train(path,log):
     net.fit_generator(generator        = _generator(256, X, y, shape, path, proportion),
                       validation_data  = _generator(20, X_val, y_val, shape, path, proportion),
                       validation_steps = 20, 
-                      epochs = 5, steps_per_epoch=50)
+                      epochs = 2, steps_per_epoch=50)
     test_idx, _ = sample_idx(30, y, proportion) 
-    pred = net.predict(X[test_idx])
-    for i, j in zip(pred, y[test_idx]):
-        print("Pred: ", i, " True: ", j)
+    for i in test_idx:
+        img, _ = image_handling(path + os.sep + X[i], 0, 0, shape)
+        img = np.reshape(img, (1,) + shape)
+        pred = net.predict(img)
+        print("Pred: ", pred, " True: ", y[i])
     net.save('testmodel3.h5')
     
 
@@ -134,7 +144,12 @@ if __name__ == "__main__":
     path = os.getcwd().split(os.sep)[:-1]
     log = path + ["driving_log.csv"]
     img = path + ["IMG"]
-    train((os.sep).join(img), (os.sep).join(log))
-
+    #train((os.sep).join(img), (os.sep).join(log))
+    front, left, right = np.loadtxt(log, delimiter=",", usecols=[0,1,2], dtype="str", unpack=True)
+    shape = (75,320,3)
+    img, _ = image_handling(path + os.sep + front[0], 1,0, shape=shape)
+    fig = plt.figure()
+    plt.imshow(img)
+    fig.savefig("test.jpg")
 
 
